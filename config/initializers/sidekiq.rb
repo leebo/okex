a7ee9dc6e7c2effd1f4a7a189eb3936d15e77d23
leebo <me@bobo.im>
2017-12-04 14:11:59 +0800

    fix ten second worker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,12 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://127.0.0.1:6379/14' }
-  config.average_scheduled_poll_interval = 10
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://127.0.0.1:6379/14' }
 end
+
+Sidekiq.options[:poll_interval] = 1
 schedule_file = "config/schedule.yml"
 
 if File.exists?(schedule_file) && Sidekiq.server?
